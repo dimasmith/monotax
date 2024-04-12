22d33b220c4f0463560723cddbb130b34b03db14
Dmytro Kovalchuk <dimasmith@gmail.com>
2024-04-13 01:45:02 +0300

    style(report): apply formatting to console renderer

--- src/report/console.rs
+++ src/report/console.rs
@@ -4,7 +4,7 @@ use std::io::Write;
 
 use super::QuarterReport;
 
-pub fn pretty_print(report: &[QuarterReport], mut writer: impl Write) -> anyhow::Result<()>{
+pub fn pretty_print(report: &[QuarterReport], mut writer: impl Write) -> anyhow::Result<()> {
     let delimiter = "-".repeat(80);
     writeln!(&mut writer, "{}", delimiter)?;
     let mut total = 0.0;
@@ -15,8 +15,18 @@ pub fn pretty_print(report: &[QuarterReport], mut writer: impl Write) -> anyhow:
         writeln!(&mut writer, "{} {}", line.year(), line.quarter())?;
         writeln!(&mut writer, "{}", "-".repeat(7))?;
         writeln!(&mut writer, "\t\tTotal\t\t\tCumulative")?;
-        writeln!(&mut writer, "Income\t\t{:.2}\t\t{:.2}", line.total_income(), line.cumulative_income())?;
-        writeln!(&mut writer, "Tax\t\t{:.2}\t\t{:.2}", line.total_tax(), line.cumulative_tax())?;
+        writeln!(
+            &mut writer,
+            "Income\t\t{:.2}\t\t{:.2}",
+            line.total_income(),
+            line.cumulative_income()
+        )?;
+        writeln!(
+            &mut writer,
+            "Tax\t\t{:.2}\t\t{:.2}",
+            line.total_tax(),
+            line.cumulative_tax()
+        )?;
         writeln!(&mut writer, "{}", delimiter)?;
     }
     writeln!(&mut writer, "Total:\t\t{:.2}\t\t{:.2}", total, tax)?;
