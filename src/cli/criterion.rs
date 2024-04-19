3a4afc618a034f7557849dc72198c95012af8261
Dmytro Kovalchuk <dimasmith@gmail.com>
2024-04-19 15:48:21 +0300

    refactor(cli): safely read predicates from cli

--- src/cli/criterion.rs
+++ src/cli/criterion.rs
@@ -6,7 +6,7 @@ use super::{
 };
 
 pub fn build_criteria(cli: &Cli) -> anyhow::Result<Vec<Box<dyn Criterion>>> {
-    let quarter_filter = build_quarter_filter(cli)?;
-    let year_filter = build_year_filter(cli)?;
+    let quarter_filter = build_quarter_filter(cli);
+    let year_filter = build_year_filter(cli);
     Ok(vec![Box::new(quarter_filter), Box::new(year_filter)])
 }
--- src/cli/filter.rs
+++ src/cli/filter.rs
@@ -4,29 +4,36 @@ use crate::cli::{IncludeQuarters, IncludeYears};
 
 use super::Cli;
 
-pub fn build_quarter_filter(cli: &Cli) -> anyhow::Result<QuarterFilter> {
-    let include_quarters = cli.include_quarters;
-    let quarter = cli.quarter;
-
-    let quarter_filter = match (include_quarters, quarter) {
-        (IncludeQuarters::Any, None) => QuarterFilter::Any,
-        (IncludeQuarters::Any, Some(q)) => QuarterFilter::Only(q),
-        (IncludeQuarters::One, None) => QuarterFilter::Current,
-        (IncludeQuarters::One, Some(q)) => QuarterFilter::Only(q),
-        (IncludeQuarters::Ytd, None) => QuarterFilter::CurrentToDate,
-        (IncludeQuarters::Ytd, Some(q)) => QuarterFilter::Ytd(q),
-    };
-    Ok(quarter_filter)
+pub fn build_quarter_filter(cli: &Cli) -> QuarterFilter {
+    QuarterFilter::from(cli)
 }
 
-pub fn build_year_filter(cli: &Cli) -> anyhow::Result<YearFilter> {
-    let year_filter = match (cli.include_years, cli.year) {
-        (IncludeYears::All, None) => YearFilter::Any,
-        (IncludeYears::All, Some(y)) => YearFilter::One(y),
-        (IncludeYears::One, None) => YearFilter::Current,
-        (IncludeYears::One, Some(y)) => YearFilter::One(y),
-        (IncludeYears::Current, None) => YearFilter::Current,
-        (IncludeYears::Current, Some(y)) => YearFilter::One(y),
-    };
-    Ok(year_filter)
+pub fn build_year_filter(cli: &Cli) -> YearFilter {
+    YearFilter::from(cli)
+}
+
+impl From<&Cli> for QuarterFilter {
+    fn from(cli: &Cli) -> Self {
+        match (cli.include_quarters, cli.quarter) {
+            (IncludeQuarters::Any, None) => QuarterFilter::Any,
+            (IncludeQuarters::Any, Some(q)) => QuarterFilter::Only(q),
+            (IncludeQuarters::One, None) => QuarterFilter::Current,
+            (IncludeQuarters::One, Some(q)) => QuarterFilter::Only(q),
+            (IncludeQuarters::Ytd, None) => QuarterFilter::CurrentToDate,
+            (IncludeQuarters::Ytd, Some(q)) => QuarterFilter::Ytd(q),
+        }
+    }
+}
+
+impl From<&Cli> for YearFilter {
+    fn from(cli: &Cli) -> Self {
+        match (cli.include_years, cli.year) {
+            (IncludeYears::All, None) => YearFilter::Any,
+            (IncludeYears::All, Some(y)) => YearFilter::One(y),
+            (IncludeYears::One, None) => YearFilter::Current,
+            (IncludeYears::One, Some(y)) => YearFilter::One(y),
+            (IncludeYears::Current, None) => YearFilter::Current,
+            (IncludeYears::Current, Some(y)) => YearFilter::One(y),
+        }
+    }
 }
