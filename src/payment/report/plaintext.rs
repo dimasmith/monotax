d349cf5f96386e86fb4fc58723152eb74eb73357
Dmytro Kovalchuk <dimasmith@gmail.com>
2024-04-26 00:41:18 +0300

    feat(payments): improve report formatting

--- src/payment/report/plaintext.rs
+++ src/payment/report/plaintext.rs
@@ -4,18 +4,21 @@ use std::io::Write;
 
 use super::PaymentReport;
 
+/// Render plaintext report on payments.
 pub fn plaintext_report<W>(report: &PaymentReport, mut writer: W) -> anyhow::Result<()>
 where
     W: Write,
 {
+    writeln!(&mut writer, "No\tDate\t\tIncome\t\tTax\t\tPaid")?;
     for (i, payment) in report.payments().iter().enumerate() {
         let date = payment.income().date();
         let income_amount = payment.income().amount();
         let tax_amount = payment.tax_amount();
-        let paid = payment.paid();
+        let paid = if payment.paid() { "Yes" } else { "No" };
+
         writeln!(
             &mut writer,
-            "{}:\t{}\t{:.2}\t{:.2}\t{}",
+            "{}:\t{}\t{:10.2}\t{:10.2}\t{}",
             i + 1,
             date,
             income_amount,
@@ -24,7 +27,7 @@ where
         )?;
     }
     writeln!(&mut writer, "{}", "-".repeat(40))?;
-    writeln!(&mut writer, "Tax dept:\t{}", report.total_debt())?;
+    writeln!(&mut writer, "\t\t\tTax debt:\t{:10.2}", report.total_debt())?;
 
     Ok(())
 }
