f6fb36c069edd9c9aea0506403f7258c86d7d964
Dmytro Kovalchuk <dimasmith@gmail.com>
2024-05-10 00:54:18 +0300

    fix(db): remove unused feature toggle

--- src/init.rs
+++ src/init.rs
@@ -1,10 +1,10 @@
 //! Initialize the application configuration
 
 use crate::config::create_default_config;
+use crate::db::init::initialize_db;
 
 pub fn init(force: bool) -> anyhow::Result<()> {
     create_default_config(force)?;
-    #[cfg(feature = "sqlite")]
-    crate::db::init::initialize_db()?;
+    initialize_db()?;
     Ok(())
 }
