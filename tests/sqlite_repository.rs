d082a8c5532a7bd03bdbbab02fd01aed70448fe1
Dmytro Kovalchuk <dimasmith@gmail.com>
2024-07-11 01:31:24 +0300

    refactor: use income repository

--- tests/sqlite_repository.rs
+++ tests/sqlite_repository.rs
@@ -1,8 +1,7 @@
 use crate::common::connect_to_test_db;
 use chrono::NaiveDateTime;
-use monotax::db::repository::find_incomes;
-use monotax::db::repository::load_all_incomes;
-use monotax::db::repository::save_incomes;
+use monotax::db::rusqlite::income::RusqliteIncomeRepository;
+use monotax::db::IncomeRepository;
 use monotax::domain::income::Income;
 use monotax::income::criteria::IncomeCriteria;
 use monotax::income::criteria::IncomeCriterion;
@@ -17,17 +16,22 @@ fn income(datetime: &str, amount: f64) -> Income {
     Income::new(datetime, amount)
 }
 
+async fn create_repository() -> RusqliteIncomeRepository {
+    let conn = connect_to_test_db().await;
+    RusqliteIncomeRepository::new(conn)
+}
+
 #[tokio::test]
 async fn save_and_load_incomes() {
-    let mut conn = connect_to_test_db().await;
+    let mut repo = create_repository().await;
 
     let income1 = income("2024-04-13 14:00:00", 225.0);
     let income2 = income("2024-07-13 14:00:00", 325.0);
     let incomes = vec![income1.clone(), income2.clone()];
 
-    let updated = save_incomes(&mut conn, &incomes).unwrap();
+    let updated = repo.save_all(&incomes).await.unwrap();
 
-    let incomes = load_all_incomes(&mut conn).unwrap();
+    let incomes = repo.find_all().await.unwrap();
     assert_eq!(incomes.len(), 2, "unexpected number of incomes");
     assert_eq!(updated, 2, "unexpected number of updated rows");
     // incomes must be ordered by date
@@ -37,7 +41,7 @@ async fn save_and_load_incomes() {
 
 #[tokio::test]
 async fn ignore_duplicate_incomes() {
-    let mut conn = connect_to_test_db().await;
+    let mut repo = create_repository().await;
 
     let income1 = income("2024-04-13 14:00:00", 225.0);
     let income2 = income("2024-07-13 14:00:00", 325.0);
@@ -45,9 +49,9 @@ async fn ignore_duplicate_incomes() {
     let income1_dup = income1.clone();
     let incomes = vec![income1.clone(), income2.clone(), income2_dup, income1_dup];
 
-    let updated = save_incomes(&mut conn, &incomes).unwrap();
+    let updated = repo.save_all(&incomes).await.unwrap();
 
-    let incomes = load_all_incomes(&mut conn).unwrap();
+    let incomes = repo.find_all().await.unwrap();
     // duplicates must be ignored
     // so we expect only 2 unique incomes
     assert_eq!(updated, 2, "unexpected number of updated rows");
@@ -59,7 +63,7 @@ async fn ignore_duplicate_incomes() {
 
 #[tokio::test]
 async fn filter_incomes_on_quarters() {
-    let mut conn = connect_to_test_db().await;
+    let mut repo = create_repository().await;
 
     let q1_2024 = income("2024-01-13 14:00:00", 125.0);
     let q2_2024 = income("2024-04-13 14:00:00", 225.0);
@@ -72,30 +76,27 @@ async fn filter_incomes_on_quarters() {
         q4_2024.clone(),
     ];
 
-    let _ = save_incomes(&mut conn, &incomes).unwrap();
+    let _ = repo.save_all(&incomes).await.unwrap();
 
     let q3_only = QuarterFilter::Only(Quarter::Q3);
-    let filtered_incomes = find_incomes(
-        &mut conn,
-        IncomeCriteria::new(&[IncomeCriterion::Quarter(q3_only)]),
-    )
-    .unwrap();
+    let filtered_incomes = repo
+        .find_by(IncomeCriteria::new(&[IncomeCriterion::Quarter(q3_only)]))
+        .await
+        .unwrap();
     assert_eq!(filtered_incomes, vec![q3_2024.clone()]);
 
     let q2_ytd = QuarterFilter::Ytd(Quarter::Q2);
-    let filtered_incomes = find_incomes(
-        &mut conn,
-        IncomeCriteria::new(&[IncomeCriterion::Quarter(q2_ytd)]),
-    )
-    .unwrap();
+    let filtered_incomes = repo
+        .find_by(IncomeCriteria::new(&[IncomeCriterion::Quarter(q2_ytd)]))
+        .await
+        .unwrap();
     assert_eq!(filtered_incomes, vec![q1_2024.clone(), q2_2024.clone()]);
 
     let q_any = QuarterFilter::Any;
-    let filtered_incomes = find_incomes(
-        &mut conn,
-        IncomeCriteria::new(&[IncomeCriterion::Quarter(q_any)]),
-    )
-    .unwrap();
+    let filtered_incomes = repo
+        .find_by(IncomeCriteria::new(&[IncomeCriterion::Quarter(q_any)]))
+        .await
+        .unwrap();
     assert_eq!(
         filtered_incomes,
         vec![
@@ -109,29 +110,27 @@ async fn filter_incomes_on_quarters() {
 
 #[tokio::test]
 async fn filter_incomes_on_years() {
-    let mut conn = connect_to_test_db().await;
+    let mut repo = create_repository().await;
 
     let y2023 = income("2023-01-13 14:00:00", 125.0);
     let y2024 = income("2024-01-13 14:00:00", 225.0);
     let y2025 = income("2025-01-13 14:00:00", 325.0);
     let incomes = vec![y2023.clone(), y2024.clone(), y2025.clone()];
 
-    let _ = save_incomes(&mut conn, &incomes).unwrap();
+    let _ = repo.save_all(&incomes).await.unwrap();
 
     let y2024_only = YearFilter::One(2024);
-    let filtered_incomes = find_incomes(
-        &mut conn,
-        IncomeCriteria::new(&[IncomeCriterion::from(y2024_only)]),
-    )
-    .unwrap();
+    let filtered_incomes = repo
+        .find_by(IncomeCriteria::new(&[IncomeCriterion::from(y2024_only)]))
+        .await
+        .unwrap();
     assert_eq!(filtered_incomes, vec![y2024.clone()]);
 
     let y_any = YearFilter::Any;
-    let filtered_incomes = find_incomes(
-        &mut conn,
-        IncomeCriteria::new(&[IncomeCriterion::from(y_any)]),
-    )
-    .unwrap();
+    let filtered_incomes = repo
+        .find_by(IncomeCriteria::new(&[IncomeCriterion::from(y_any)]))
+        .await
+        .unwrap();
     assert_eq!(
         filtered_incomes,
         vec![y2023.clone(), y2024.clone(), y2025.clone()]
@@ -140,7 +139,7 @@ async fn filter_incomes_on_years() {
 
 #[tokio::test]
 async fn filter_incomes_on_quarters_and_years() {
-    let mut conn = connect_to_test_db().await;
+    let mut repo = create_repository().await;
 
     let q1_2023 = income("2023-01-13 14:00:00", 125.0);
     let q2_2023 = income("2023-04-13 14:00:00", 225.0);
@@ -161,30 +160,28 @@ async fn filter_incomes_on_quarters_and_years() {
         q4_2024.clone(),
     ];
 
-    let _ = save_incomes(&mut conn, &incomes).unwrap();
+    let _ = repo.save_all(&incomes).await.unwrap();
 
     let q3_2024_only = QuarterFilter::Only(Quarter::Q3);
     let y2024_only = YearFilter::One(2024);
-    let filtered_incomes = find_incomes(
-        &mut conn,
-        IncomeCriteria::new(&[
+    let filtered_incomes = repo
+        .find_by(IncomeCriteria::new(&[
             IncomeCriterion::from(q3_2024_only),
             IncomeCriterion::from(y2024_only),
-        ]),
-    )
-    .unwrap();
+        ]))
+        .await
+        .unwrap();
     assert_eq!(filtered_incomes, vec![q3_2024.clone()]);
 
     // test for ytd q3 in 2023
     let q3_ytd_2023 = QuarterFilter::Ytd(Quarter::Q3);
     let y2023_only = YearFilter::One(2023);
-    let filtered_incomes = find_incomes(
-        &mut conn,
-        IncomeCriteria::new(&[
+    let filtered_incomes = repo
+        .find_by(IncomeCriteria::new(&[
             IncomeCriterion::from(q3_ytd_2023),
             IncomeCriterion::from(y2023_only),
-        ]),
-    );
+        ]))
+        .await;
     assert_eq!(
         filtered_incomes.unwrap(),
         vec![q1_2023.clone(), q2_2023.clone(), q3_2023.clone()]
@@ -193,10 +190,12 @@ async fn filter_incomes_on_quarters_and_years() {
     // test for q4 in any year
     let q4_only = QuarterFilter::Only(Quarter::Q4);
     let y_any = YearFilter::Any;
-    let filtered_incomes = find_incomes(
-        &mut conn,
-        IncomeCriteria::new(&[IncomeCriterion::from(q4_only), IncomeCriterion::from(y_any)]),
-    );
+    let filtered_incomes = repo
+        .find_by(IncomeCriteria::new(&[
+            IncomeCriterion::from(q4_only),
+            IncomeCriterion::from(y_any),
+        ]))
+        .await;
     assert_eq!(
         filtered_incomes.unwrap(),
         vec![q4_2023.clone(), q4_2024.clone()]
@@ -205,13 +204,12 @@ async fn filter_incomes_on_quarters_and_years() {
     // test for whole year 2024 regardless of quarter
     let y2024_only = YearFilter::One(2024);
     let q_ay = QuarterFilter::Any;
-    let filtered_incomes = find_incomes(
-        &mut conn,
-        IncomeCriteria::new(&[
+    let filtered_incomes = repo
+        .find_by(IncomeCriteria::new(&[
             IncomeCriterion::from(q_ay),
             IncomeCriterion::from(y2024_only),
-        ]),
-    );
+        ]))
+        .await;
     assert_eq!(
         filtered_incomes.unwrap(),
         vec![
