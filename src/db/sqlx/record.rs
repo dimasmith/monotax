5dae164ba7e4eed07846c1e0a81b6d9ddb45b999
Dmytro Kovalchuk <dimasmith@gmail.com>
2024-07-20 11:18:46 +0300

    feat: store incomes using sqlx

--- src/db/sqlx/record.rs
+++ src/db/sqlx/record.rs
@@ -1,6 +1,6 @@
-use chrono::NaiveDateTime;
+use chrono::{Datelike, NaiveDateTime};
 
-use crate::domain::income::Income;
+use crate::{domain::income::Income, time::Quarter};
 
 pub struct IncomeRecord {
     pub date: NaiveDateTime,
@@ -15,5 +15,20 @@ pub struct IncomeRecord {
 impl From<IncomeRecord> for Income {
     fn from(record: IncomeRecord) -> Self {
         Income::new(record.date, record.amount).with_no(record.payment_no)
+    }
+}
+
+impl From<&Income> for IncomeRecord {
+    fn from(value: &Income) -> Self {
+        let quarter = Quarter::from(&value.datetime()).index();
+        Self {
+            date: value.datetime(),
+            amount: value.amount(),
+            payment_no: value.income_no(),
+            description: value.comment().map(|s| s.to_string()),
+            year: value.datetime().year() as u16,
+            quarter: quarter as u8,
+            tax_paid: false,
+        }
     }
 }
